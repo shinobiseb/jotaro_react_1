26b333819dbdf287dc15dc97c5ca3b72add7c40d
shinobiseb <sebpatin@gmail.com>
2023-05-27 14:05:28 -0400

    Contact component created

--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Contact.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Contact.tsx
@@ -2,6 +2,17 @@ import React from 'react'
 
 export default function Contact() {
   return (
-    <div>Contact</div>
+    <div className='flex flex-col w-full h-full mt-20 items-center'>
+        <h3 className='text-3xl contact-header'>Email me!</h3>
+            <section> jotarokujo@gmail.com </section>
+
+        <h3 className='text-3xl contact-header'>My Social Medias</h3>
+        <ul className='flex flex-col items-center text-xl'>
+            <li> 
+                <a className='contact-link' target='_blank' href='https://www.instagram.com/pichiru__/'> Instagram</a> </li> 
+            <li> 
+                <a className='contact-link' href='#'> Twitter</a> </li> 
+        </ul>
+    </div>
   )
 }
