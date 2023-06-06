8410ebc1fbc60f15cdabe98e67d4b7ce8306ac14
shinobiseb <sebpatin@gmail.com>
2023-06-06 10:40:32 -0400

    removed spawn dummies function

--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Gallery.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Gallery.tsx
@@ -3,28 +3,6 @@ import { useEffect } from 'react'
 
 export default function Gallery() {
 
-  useEffect(() => {
-  
-  const gallery = document.getElementById("Gallery")
-
-  function spawnDummies() {
-
-    for(let i = 0; i < 10; i++) {
-        //create a div
-        let newDiv = document.createElement('div')
-        //give div id of i
-        newDiv.setAttribute("id", `div${i}`)
-        newDiv.setAttribute("class", `dummyDiv`)
-        //append div to gallery
-        gallery?.appendChild(newDiv)
-    }
-  }
-
-    spawnDummies()
-  }, []);
-
-  
-
   return (
     
     <div id='Gallery' className='flex flex-wrap flex-row h-screen w-full border-solid justify-around p-6'>
