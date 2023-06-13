68042c865c06e7bd4db7195a8af54014a12fe093
shinobiseb <sebpatin@gmail.com>
2023-06-13 16:14:56 -0400

    modal working

--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Animations.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Animations.tsx
@@ -1,33 +1,43 @@
-import React from 'react'
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { animationsLinks } from '../assets/Imageurls';
-import {imgSpawner} from '../assets/Imageurls';
+import { imgSpawner } from '../assets/Imageurls';
 import Modal from './Modal';
 
-
 export default function Animations() {
+  const [modalState, setModalState] = useState(false);
+  const [selectedContent, setSelectedContent] = useState('');
 
-  const [modalstate, setModalState] = useState(false)
+  useEffect(() => {
+    const container = document.getElementById('AnimationCont');
+    imgSpawner(animationsLinks, container);
 
-    useEffect(()=> {
-      const container = document.getElementById('AnimationCont')
-      imgSpawner(animationsLinks, container)
+    const handleClick = (event : any) => {
+      const dataContent = event.target.getAttribute('src');
+      setSelectedContent(dataContent);
+      setModalState(true);
+    };
 
-      if(container) {
-        for (const child of container?.children) {
-          child.addEventListener('click', ()=> setModalState(!modalstate))
-        }
-      } else {
-        console.warn('container not found')
+    if (container) {
+      for (const child of container.children) {
+        child.addEventListener('click', handleClick);
       }
+    }
+
+    return () => {
+      if (container) {
+        for (const child of container.children) {
+          child.removeEventListener('click', handleClick);
+        }
+      }
+    };
   }, []);
 
   return (
-  <>
-    <Modal state={modalstate} setState={setModalState}/>
-    <button></button>
-    <div className='h-full w-full flex flex-row flex-wrap justify-center' id='AnimationCont'>
-    </div>
-  </>
-  )
+    <>
+      <Modal content={selectedContent} state={modalState} setState={setModalState} />
+      <button></button>
+      <div className='h-full w-full flex flex-row flex-wrap justify-center' id='AnimationCont'>
+      </div>
+    </>
+  );
 }
--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Modal.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Modal.tsx
@@ -1,24 +1,33 @@
-import React from 'react'
-import { useState } from 'react'
+import React from 'react';
 
 type modalProps = {
-    state : boolean
-    setState : React.Dispatch<React.SetStateAction<boolean>> 
-}
+  state: boolean;
+  setState: React.Dispatch<React.SetStateAction<boolean>>;
+  content: any;
+};
 
-export default function Modal(props : modalProps) {
+export default function Modal(props: modalProps) {
 
-    return (props.state) ? 
-    (
+console.log(props)
+
+  const handleClose = () => {
+    props.setState(false);
+  };
+
+  return props.state ? (
     <div className='fixed'>
-            <div className='fixed flex items-center justify-center z-3 h-screen w-screen overlay'>
-                <div className='flex flex-col items-center justify-center w-5/6 h-3/4 border bg-slate drop-shadow-md z-5 fixed modal'>
-                    <i className="gg-close close-button mt-3"
-                    onClick={()=> props.setState(!props.state)}>
-                    </i>
-                </div>
-            </div> 
+      <div className='fixed flex items-center justify-center z-3 h-screen w-screen overlay'>
+        <div className='flex flex-col items-center justify-center w-5/6 h-3/4 border bg-slate drop-shadow-md z-5 fixed modal'>
+          <i className='gg-close close-button mt-3' onClick={handleClose}></i>
+          <div className='modalContainer w-full h-auto border'>
+            {typeof props.content === 'string' && props.content.includes('.mp4') ? (
+              <video src={props.content} controls autoPlay />
+            ) : (
+              <img src={props.content} alt='Modal Content' />
+            )}
+          </div>
+        </div>
+      </div>
     </div>
-    ) 
-    : null;
+  ) : null;
 }
