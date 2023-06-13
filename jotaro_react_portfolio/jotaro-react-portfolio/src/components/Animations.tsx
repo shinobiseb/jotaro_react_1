44e8c0cc0474b9e7a1a38a8e35bea8af9ea0ba89
shinobiseb <sebpatin@gmail.com>
2023-06-13 14:52:08 -0400

    added event listeners

--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Animations.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Animations.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { animationsLinks } from '../assets/Imageurls';
 import {imgSpawner} from '../assets/Imageurls';
 import Modal from './Modal';
@@ -7,16 +7,24 @@ import Modal from './Modal';
 
 export default function Animations() {
 
-  
+  const [modalstate, setModalState] = useState(false)
 
     useEffect(()=> {
       const container = document.getElementById('AnimationCont')
       imgSpawner(animationsLinks, container)
+
+      if(container) {
+        for (const child of container?.children) {
+          child.addEventListener('click', ()=> setModalState(!modalstate))
+        }
+      } else {
+        console.warn('container not found')
+      }
   }, []);
 
   return (
   <>
-    <Modal/>
+    <Modal state={modalstate} setState={setModalState}/>
     <button></button>
     <div className='h-full w-full flex flex-row flex-wrap justify-center' id='AnimationCont'>
     </div>
--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Modal.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Modal.tsx
@@ -1,27 +1,24 @@
 import React from 'react'
 import { useState } from 'react'
 
-export default function Modal() {
+type modalProps = {
+    state : boolean
+    setState : React.Dispatch<React.SetStateAction<boolean>> 
+}
 
-    const [modalstate, setModalState] = useState(true)
+export default function Modal(props : modalProps) {
 
-    return (
+    return (props.state) ? 
+    (
     <div className='fixed'>
-
-        {
-        modalstate ? 
             <div className='fixed flex items-center justify-center z-3 h-screen w-screen overlay'>
-
                 <div className='flex flex-col items-center justify-center w-5/6 h-3/4 border bg-slate drop-shadow-md z-5 fixed modal'>
-    
                     <i className="gg-close close-button mt-3"
-                    onClick={()=> setModalState(!modalstate)}>
+                    onClick={()=> props.setState(!props.state)}>
                     </i>
                 </div>
-            </div> :
-        null
-        }
-
+            </div> 
     </div>
-  )
+    ) 
+    : null;
 }
