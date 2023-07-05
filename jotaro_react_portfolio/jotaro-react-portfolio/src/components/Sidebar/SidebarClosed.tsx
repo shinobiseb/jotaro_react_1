f46afc13f5b9b48c507c4bf69d5e8ee2b3d37aba
shinobiseb <sebpatin@gmail.com>
2023-07-05 16:26:31 -0400

    center icon a bit

--- jotaro_react_portfolio/jotaro-react-portfolio/src/components/Sidebar/SidebarClosed.tsx
+++ jotaro_react_portfolio/jotaro-react-portfolio/src/components/Sidebar/SidebarClosed.tsx
@@ -7,11 +7,11 @@ type stateprops = {
 export default function SidebarClosed( state : stateprops ) {
 
   return (
-    <div className='bg-light-pink p-3 w-full sm:h-full sm:w-10 sm:p-0 fixed top-0 align-center items-center border-b-[1px]'>
+    <div className='bg-light-pink p-3 w-screen sm:h-full sm:w-10 sm:p-0 fixed top-0 align-center items-center border-b-[1px] z-20'>
       
         <button 
         onClick={state.state} 
-        className='p-2'>
+        className='p-2 sm:pt-4 flex items-center'>
           <i className="gg-menu"></i>
         </button>
       
